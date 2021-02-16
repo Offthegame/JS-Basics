0d370e0e133fd026aae422e45bc740ab77df018f
Dongha Yi <ceokingmaker@naver.com>
2021-02-16 15:13:54 +0900

    background changes

--- todo.js
+++ todo.js
@@ -30,7 +30,7 @@ function paintToDo(text) {
     const delBtn = document.createElement("button");
     const span = document.createElement("span");
     const newId = toDos.length + 1;
-    delBtn.innerText = "❌";
+    delBtn.innerText = "삭제";
     delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
     li.appendChild(delBtn);
